e3cc851e706522966e3436e2981d92ca410f3d9a
hoream <hoream@qq.com>
2024-08-28 21:41:56 +0800

    add arrow
    add arrow

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -163,6 +163,14 @@ require("lazy").setup({
 	-- 		require("config.bufferline")
 	-- 	end,
 	-- },
+	{
+		"otavioschwanck/arrow.nvim",
+		opts = {
+			show_icons = true,
+			leader_key = ";", -- Recommended to be a single key
+			buffer_leader_key = "m", -- Per Buffer Mappings
+		},
+	},
 
 	{
 		"folke/trouble.nvim",
