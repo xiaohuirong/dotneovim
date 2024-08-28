4217ff80751fd86178a3a7f31b62a013228561df
hoream <hoream@qq.com>
2024-08-28 16:23:03 +0800

    add better ui

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,6 +37,24 @@ require("lazy").setup({
 		version = "v2.*",
 	},
 	"tanvirtin/monokai.nvim",
+	-- Better UI
+	-- Run `:checkhealth noice` to check for common issues
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
