f0582dfcd6aff891af06e52c361b750912bacef2
hoream <hoream@qq.com>
2024-10-05 14:39:51 +0800

    let python use externTerminal

--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -110,6 +110,9 @@ dap.configurations.python = {
 		-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
 		program = "${file}", -- This configuration will launch the current file if used.
+		-- console = "integratedTerminal",
+		console = "externalTerminal",
+
 		pythonPath = function()
 			-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
 			-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
